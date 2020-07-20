9839916181702cbd4a78569c7c6c01e96610386c
mag389 <mag389@cornell.edu>
2020-07-20 18:17:34 +0000

    preliminary balance function. tested local, but may have mistake

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+size_t binary_tree_height(const binary_tree_t *tree);
+/**
+* binary_tree_balance - finds balance of the binary tree
+* Return: the int balance 
+* @tree: the root node to test balance of
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
+
+/**
+* binary_tree_height - finds the height of the binary tree
+* Return: the height as a size_t
+* @tree: the root node
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h_left, h_right;
+
+	if (!tree)
+		return (0);
+	if (!(tree->left) && !(tree->right))
+		return (1);
+	h_left = binary_tree_height(tree->left);
+	h_right = binary_tree_height(tree->right);
+	if (h_left > h_right)
+		return (1 + h_left);
+	return (1 + h_right);
+}
