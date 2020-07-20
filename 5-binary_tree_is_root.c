80aa1681e55a9d866abaed4b6475dc24b4ac58ff
mag389 <mag389@cornell.edu>
2020-07-20 16:46:22 +0000

    tetsing for root/leaf status

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_root - tests if the node is a root
+* Return: 1 for root else 0
+* @node: the node to test
+*/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (!(node->parent))
+		return (1);
+	return (0);
+}
