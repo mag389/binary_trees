f523fbdfec869916590fb6419479338f6ba10fd0
mag389 <mag389@cornell.edu>
2020-07-22 16:54:00 +0000

    added null check for tree

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -9,13 +9,19 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *temp;
 
+	if (!tree)
+		return (NULL);
 	temp = tree->right;
 	if (!(tree->right))
 		return (tree);
+/*	NULL check*/
 	tree->right = temp->left;
 	if (tree->right)
 		tree->right->parent = tree;
+/*	dael with tree right the switching child*/
 	temp->parent = tree->parent;
+	tree->parent = temp;
+/*	switch parents */
 	if (temp->parent)
 	{
 		if (temp->parent->right == tree)
@@ -23,7 +29,7 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 		else
 			temp->parent->left = tree;
 	}
-	tree->parent = temp;
+/*	change parent to point to new spot*/
 	temp->left = tree;
 	return (temp);
 }
