1ca7d45825fede2f739ce07eb9e7c7b9196b218d
mag389 <mag389@cornell.edu>
2020-07-23 05:47:28 +0000

    removed a bunch of prints and compacted the tree check

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -10,7 +10,7 @@ avl_t *avl_insert(avl_t **tree, int value)
 {
 	avl_t *newt, *temp, *temp2, *ncheck;
 
-	printf("tying to insert %i\n", value);
+/*	printf("tying to insert %i\n", value);*/
 	if (!tree)
 		return (NULL);
 	if (!(*tree))
@@ -28,23 +28,13 @@ avl_t *avl_insert(avl_t **tree, int value)
 			(*tree)->left = binary_tree_node(*tree, value);
 			newt = (*tree)->left;
 			temp = rebalance((*tree)->left);
-/*			if (temp)
-				printf("temp issa %i\n", temp->n);
-			else
-				printf("apparently tmep is null for the rebalance here\n");
-			if (temp && !(temp->parent))
-				printf("##########################\n");
-			while ((*tree)->parent && 0)
-				*tree = (*tree)->parent;
-			if (temp && temp->parent == NULL && (*tree)->parent == NULL)
-				*tree = temp;*/
 			return (newt);
 		}
 		else
 		{
 			temp = avl_insert((&(*tree)->left), value);
-/*			if (temp && !(temp->parent) && (*tree)->parent == NULL)
-				*tree = temp;*/
+			while ((*tree)->parent != NULL && !ncheck)
+				*tree = (*tree)->parent;
 			return (temp);
 		}
 	}
@@ -52,52 +42,28 @@ avl_t *avl_insert(avl_t **tree, int value)
 	{
 		if ((*tree)->right == NULL)
 		{
-			printf("is 514 here\n");
 			(*tree)->right = binary_tree_node(*tree, value);
 			newt = (*tree)->right;
 			temp = rebalance((*tree)->right);
-/*                      if (temp)
-                                printf("temp issa %i\n", temp->n);
-                        else
-                                printf("apparently tmep is null for the rebalance here\n");
-                        if (temp && !(temp->parent))
-			{
-                                printf("********************************\n");
-				printf("tree is %i\n", (*tree)->n);
-				while ((*tree)->parent)
-					*tree = (*tree)->parent;
-			}
-			if (temp && temp->parent == NULL && (*tree)->parent == NULL)
-				*tree = temp;
-*/
 			return (newt);
 		}
 		else
 		{
-			printf("we're in the else!!!!!!!\n");
 			temp = avl_insert((&(*tree)->right), value);
-			printf("I think here's where we need to change\n");
 			temp2 = temp;
 			while (temp2 && temp2->parent)
 			{
-				printf("++++++++++++++++++++++++++++++\n");
-				binary_tree_print(temp2);
+/*				printf("++++++++++++++++++++++++++++++\n");*/
+/*				binary_tree_print(temp2);*/
 				temp2 = temp2->parent;
-				printf("===============================\n");
+/*				printf("===============================\n");*/
 			}
-			printf("is the where it segafults\n");
-			if (temp2 && *tree && !(temp2->parent) && (*tree)->parent && !ncheck)
-				*tree = temp2;
-			printf("nope %i\n", temp2->n);
-			
-/*			if (temp && !(temp->parent) && !((*tree)->parent))
-			{
-				*tree = temp;
-				printf("actually changed in the other section\n");
-			}
+/*
+*			if (temp2 && *tree && !(temp2->parent) && (*tree)->parent && !ncheck)
+*				*tree = temp2;
 */
-/*			while ((*tree)->parent != NULL)
-				*tree = (*tree)->parent;*/
+			while ((*tree)->parent != NULL && !ncheck)
+				*tree = (*tree)->parent;
 			return (temp);
 		}
 	}
@@ -117,8 +83,8 @@ avl_t *rebalance(avl_t *newt)
 
 	if (!newt || !(newt->parent) || !(newt->parent->parent))
 		return (NULL);
-	printf("--------------------------------------------\n");
-	binary_tree_print(newt->parent->parent);
+/*	printf("--------------------------------------------\n");*/
+/*	binary_tree_print(newt->parent->parent);*/
 	balance = binary_tree_balance(newt->parent->parent);
 	if (balance <= 1 && balance >= -1)
 		return (rebalance(newt->parent));
