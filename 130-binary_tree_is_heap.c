ab79d8b1f2c0ead862760c2b2785a6cc90b7b48a
mag389 <mag389@cornell.edu>
2020-07-23 19:44:29 +0000

    added an aditional size check

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -15,6 +15,8 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 		return (0);
 	if (binary_tree_height(tree->left) < binary_tree_height(tree->right))
 		return (0);
+	if (binary_tree_size(tree->left) < binary_tree_size(tree->right))
+		return (0);
 	if (tree->left && tree->left->n > tree->n)
 		return (0);
 	if (tree->right && tree->right->n > tree->n)
@@ -33,10 +35,21 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (!(tree->left) && !(tree->right))
-		return (0);
+		return (1);
 	h_left = binary_tree_height(tree->left);
 	h_right = binary_tree_height(tree->right);
 	if (h_left > h_right)
 		return (1 + h_left);
 	return (1 + h_right);
+}
+/**
+* binary_tree_size - returns size of the binary tree
+* Return: size_t size of the tree
+* @tree: pointer to root node of the tree to be measured
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
