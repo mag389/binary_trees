a25dc3f94bfcf17a6d47aa04f8bfcec3c0b17721
mag389 <mag389@cornell.edu>
2020-07-23 20:07:24 +0000

    another check this time with pasted completeness

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-size_t min_height(const binary_tree_t *tree);
+
 /**
 * binary_tree_is_heap - deterines if the tree is a heap
 * Return: 1 if it is a valid max binary heap, else 0
@@ -11,13 +11,11 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 		return (0);
 	if (!(tree->right) && !(tree->left))
 		return (1);
+	if (binary_tree_is_complete(tree) == 0)
+		return (0);
 	if (!(tree->left) && tree->right)
 		return (0);
 	if (binary_tree_height(tree->left) < binary_tree_height(tree->right))
-		return (0);
-	if (binary_tree_size(tree->left) < binary_tree_size(tree->right))
-		return (0);
-	if (min_height(tree->left) < binary_tree_height(tree->right))
 		return (0);
 	if (tree->left && tree->left->n >= tree->n)
 		return (0);
@@ -26,23 +24,53 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 	return (binary_tree_is_heap(tree->left) && binary_tree_is_heap(tree->right));
 }
 /**
-* min_height - finds the height of the binary tree
-* Return: the height as a size_t
-* @tree: the root node
+* binary_tree_is_complete - tests completeness
+* Return: 1 for complete else 0
+* @tree: the root node of the tree
 */
-size_t min_height(const binary_tree_t *tree)
+int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	size_t h_left, h_right;
+	size_t max_height, i;
+	int hitleaf = 0;
 
 	if (!tree)
 		return (0);
-	if (!(tree->left) && !(tree->right))
-		return (1);
-	h_left = binary_tree_height(tree->left);
-	h_right = binary_tree_height(tree->right);
-	if (h_left < h_right)
-		return (1 + h_left);
-	return (1 + h_right);
+	max_height = binary_tree_height(tree);
+	for (i = 0; i <= max_height; i++)
+	{
+		comp_check(tree, &hitleaf, i);
+		if (hitleaf == 2)
+			return (0);
+	}
+	return (1);
+}
+/**
+* comp_check - checks if the current node can be in a complete tree
+* Return: void
+* @tree: root node
+* @hitleaf: has a leaf been hit
+* @level: what tree level
+*/
+void comp_check(const binary_tree_t *tree, int *hitleaf, size_t level)
+{
+	if (!tree)
+	{
+		if (*hitleaf == 0)
+			*hitleaf = 1;
+		return;
+	}
+	if (level == 0)
+	{
+		if (*hitleaf == 1)
+		{
+			*hitleaf = 2;
+		}
+	}
+	else
+	{
+		comp_check(tree->left, hitleaf, level - 1);
+		comp_check(tree->right, hitleaf, level - 1);
+	}
 }
 /**
 * binary_tree_height - finds the height of the binary tree
@@ -56,21 +84,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (!(tree->left) && !(tree->right))
-		return (1);
+		return (0);
 	h_left = binary_tree_height(tree->left);
 	h_right = binary_tree_height(tree->right);
 	if (h_left > h_right)
 		return (1 + h_left);
 	return (1 + h_right);
-}
-/**
-* binary_tree_size - returns size of the binary tree
-* Return: size_t size of the tree
-* @tree: pointer to root node of the tree to be measured
-*/
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
