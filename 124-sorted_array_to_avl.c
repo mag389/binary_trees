6662f8e329f3cea4f616054b953aebadc89c431b
mag389 <mag389@cornell.edu>
2020-07-23 19:21:33 +0000

    added more failure cases

--- 124-sorted_array_to_avl.c
+++ 124-sorted_array_to_avl.c
@@ -16,14 +16,19 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 	middle = size / 2 - (1 - size % 2);
 
 	new_node = binary_tree_node(NULL, array[middle]);
+	if (!new_node)
+		return (NULL);
 
 	new_node->left = sorted_array_to_avl(array, middle);
 	if (new_node->left)
 		new_node->left->parent = new_node;
-
+	else if (middle > 0)
+		return (NULL);
 	new_node->right = sorted_array_to_avl(&array[middle + 1], size / 2);
 	if (new_node->right)
 		new_node->right->parent = new_node;
+	else if (size / 2 > 0)
+		return (NULL);
 
 	return (new_node);
 }
