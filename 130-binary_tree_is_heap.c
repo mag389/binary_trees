645f61e1d7a74b659e69c0a34b37f8f152aac25f
mag389 <mag389@cornell.edu>
2020-07-23 19:50:03 +0000

    adding a min height check

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-
+size_t min_height(const binary_tree_t *tree);
 /**
 * binary_tree_is_heap - deterines if the tree is a heap
 * Return: 1 if it is a valid max binary heap, else 0
@@ -17,11 +17,32 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 		return (0);
 	if (binary_tree_size(tree->left) < binary_tree_size(tree->right))
 		return (0);
+	if (min_height(tree->left) < binary_tree_height(tree->right))
+		return (0);
 	if (tree->left && tree->left->n > tree->n)
 		return (0);
 	if (tree->right && tree->right->n > tree->n)
 		return (0);
 	return (binary_tree_is_heap(tree->left) && binary_tree_is_heap(tree->right));
+}
+/**
+* min_height - finds the height of the binary tree
+* Return: the height as a size_t
+* @tree: the root node
+*/
+size_t min_height(const binary_tree_t *tree)
+{
+	size_t h_left, h_right;
+
+	if (!tree)
+		return (0);
+	if (!(tree->left) && !(tree->right))
+		return (1);
+	h_left = binary_tree_height(tree->left);
+	h_right = binary_tree_height(tree->right);
+	if (h_left < h_right)
+		return (1 + h_left);
+	return (1 + h_right);
 }
 /**
 * binary_tree_height - finds the height of the binary tree
